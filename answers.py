c5e9ba4170af01ca64a15f98a9630b09b67b226a
@dorothykiz1 <kizdorothy@gmail.com>
2019-03-20 21:31:39 +0300

    Modify answers.py

--- answers.py
+++ answers.py
@@ -1,48 +1,47 @@
-# 1- celcius to Fahrenheit
+# # 1- celcius to Fahrenheit
 
-def convert(Celsius):
-    Fahrenheit = (Celsius * 1.8) + 32
-    print( f"The Fahrenheit temperature of Celcius temperature {Celsius} is {Fahrenheit} .")
+# def convert(Celsius):
+#     Fahrenheit = (Celsius * 1.8) + 32
+#     print( f"The Fahrenheit temperature of Celcius temperature {Celsius} is {Fahrenheit} .")
 
-convert(10)
-
-
-
-# 2- reverse string changing to a list using slicing
-def reverse_string(s):
-   list1 = list(s)[::-1]
-   #converting a list back to astring
-   print(f'the reverse of {s} is {'.join(list1)} .' )
-
-reverse_string('keza')
+# convert(10)
 
 
 
-# 3- factorial
-def find_factorial(n):
-    factorial=1
+# # 2- reverse string changing to a list using slicing
+# def reverse_string(s):
+#    list1 = list(s)[::-1]
+#    #converting a list back to astring
+#    print(f'the reverse of {s} is {'.join(list1)} .' )
 
-    if n<0:
-        print ('The number is a negative please enter a positive number!')
-    elif n==0:
-        print(f'the factorial of {n} \szis 0 ')
-
-    else:
-        for num in range(1,n+1):
-            factorial=factorial*num
-
-        print(f'the factorial of {n} is {factorial}')
-
-find_factorial(0)
+# reverse_string('keza')
 
 
 
+# # 3- factorial
+# def find_factorial(n):
+#     factorial=1
 
-#4 -length of the longest word
+#     if n<0:
+#         print ('The number is a negative please enter a positive number!')
+#     elif n==0:
+#         print(f'the factorial of {n} is 0 ')
 
-# take the string as the functional argument
-# split the string and return alist of all words separated by space
-# traverse through the list and find out the string with max length,store info in some variable
+#     else:
+#         for num in range(1,n+1):
+#             factorial=factorial*num
+
+#         print(f'the factorial of {n} is {factorial}')
+# find_factorial(0)
+
+
+
+
+# #4 -length of the longest word
+
+# # take the string as the functional argument
+# # split the string and return alist of all words separated by space
+# # traverse through the list and find out the string with max length,store info in some variable
 def length_longest(sentence):
     words = sentence.split()
     max_length= 0
@@ -51,13 +50,26 @@ def length_longest(sentence):
         if len(word)> max_length:
             max_length=len(word)
             max_word =word
-    print(max_length) 
+    print(f'the max_word is : {max_word} and the max_length is : {max_length}') 
 
-length_longest('is international')    
-
-
+length_longest('it the string and return alist of all words separated by space')    
 
 
-#5-
-        
 
+
+# # #5-
+# #     #  Return an array consisting of the largest number from each 
+# #     #  provided sub-array. For simplicity, the provided array will contain
+# #     #   exactly 4 sub-arrays. Remember, you can iterate through an array with a simple for loop,
+# #     #   and access each member with array syntax arr[i].   
+arr =input('Enter an array of numbers:')
+if arr:
+    def largest_number(arr):
+        arr2 = []
+        for list in arr:
+            # a = max(list)
+            arr2.append(max(list))
+        print(arr2)
+else:
+    print('Enter an array with sub_arrays :')
+largest_number(arr)       
